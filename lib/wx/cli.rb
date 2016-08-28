9e72959abc77b38d39807ae659372bbe3f134cc1
mcglaser <michaelcharlesglaser@gmail.com>
2016-08-28 13:36:54 -0700

    Created a get_weather method that calls the weather underground api and displays an extended forecast

--- lib/wx/cli.rb
+++ lib/wx/cli.rb
@@ -1,24 +1,47 @@
+require "rubygems"
+require "nokogiri"
+require 'open-uri'
+require 'cgi'
+
 class Wx::CLI
 
 
   def call
     greeting
     detailed_weather
+    get_weather
   end
 
   def greeting
-    puts "What's The Weather Like Today? Enter Your City Or Zip Code"
+    puts "Looking For Some Weather Info? Enter Your City Or Zip Code"
     @location = nil
     @location = gets
   end
 
   def detailed_weather
     puts @location
-        puts @location
-            puts @location
-                puts @location
-                    puts @location
-                        puts @location
+  end
+
+
+  def get_weather
+    url       = "http://api.wunderground.com/auto/wui/geo/ForecastXML/index.xml?query=#{CGI.escape(@location)}"
+    document  = Nokogiri::XML(open(url))
+    
+    forecast = document.css('fcttext')
+    
+    document.css("simpleforecast forecastday").each_with_index do |forecastday, i|
+      highs = forecastday.css('high')
+      lows  = forecastday.css('low')
+      
+      print "\n"
+      print forecastday.css("date weekday").first.content
+      print "\n"
+      print " High: #{highs.css('fahrenheit').first.content} F / #{highs.css('celsius').first.content} C \n"
+      print " Low:  #{lows.css('fahrenheit').first.content} F / #{lows.css('celsius').first.content} C   \n"
+      print " #{forecast[i].content} \n" if forecast[i]
+    end
+    
+    print "\n"
   end
 
 

cc0543d7fd27252de51e3e590e9f6da12b3caf9b
mcglaser <michaelcharlesglaser@gmail.com>
2016-08-28 12:43:07 -0700

    Added a second method to the call

--- lib/wx/cli.rb
+++ lib/wx/cli.rb
@@ -3,13 +3,22 @@ class Wx::CLI
 
   def call
     greeting
+    detailed_weather
   end
 
   def greeting
     puts "What's The Weather Like Today? Enter Your City Or Zip Code"
-    location = nil
-    location = gets
-    puts location
+    @location = nil
+    @location = gets
+  end
+
+  def detailed_weather
+    puts @location
+        puts @location
+            puts @location
+                puts @location
+                    puts @location
+                        puts @location
   end
 
 

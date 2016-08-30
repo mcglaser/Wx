6ca4287707b01db2b617f54ebbd0dc9aea4da6f2
mcglaser <michaelcharlesglaser@gmail.com>
2016-08-30 16:48:48 -0700

    Created a self.single_day_weather method which shows a single day's forecast

--- lib/wx/weather.rb
+++ lib/wx/weather.rb
@@ -7,54 +7,39 @@ class Wx::Weather
 
 # attr_accessor :day, :high, :low, :forecast
   
-  def self.test
-   self.greeting
-   self.scrape_api
-   self.select_day
-  #self.get_weather
-  end
+    def self.test
+     self.greeting
+     self.scrape_api
+     self.select_day
+     self.single_day_weather
+    end
 
 
     def self.greeting
       puts "Looking For Some Weather Info? Enter Your City Or Zip Code:"
+      self.set_location
+    end
+
+    def self.set_location
       @location = nil
-      @location = gets
+      @location = gets.strip
       puts
+      if self.invalid_city?
+        puts "We Did Not Recognize The City You Entered. Please Enter Your City Or Zip Code Again:"
+        self.set_location
+      end
     end
 
     def self.location
       @location
     end
 
-  def self.today
-    # probably don't need this method since we have #self.scrape_data
-    self.scrape_data
-  end
 
-
-
-
-  def self.scrape_data
-    data = []
-
-    data << self.scrape_api
-
-    data
-  end
-
-
-  def self.get_weather
+    def self.get_weather
       url       = "http://api.wunderground.com/auto/wui/geo/ForecastXML/index.xml?query=#{@location}"
       document  = Nokogiri::XML(open(url))
       
       forecast = document.css('fcttext')
-
-      if self.invalid_city?
-        puts "We Did Not Recognize The City You Entered. Please Enter Your City Or Zip Code Again:"
-        @location = nil
-        @location = gets
-        get_weather 
-      else
       
       document.css("simpleforecast forecastday").each_with_index do |forecastday, i|
         highs = forecastday.css('high')
@@ -69,74 +54,76 @@ class Wx::Weather
       end
       
       print "\n"
+    end
 
+    def self.invalid_city?
+      url       = "http://api.wunderground.com/auto/wui/geo/ForecastXML/index.xml?query=#{@location}"
+      document  = Nokogiri::XML(open(url))
+      document.css("simpleforecast forecastday").empty?
+    end
+
+
+
+    def self.scrape_api
+      url       = "http://api.wunderground.com/auto/wui/geo/ForecastXML/index.xml?query=#{@location}"
+      document  = Nokogiri::XML(open(url))
+      
+      @days = []
+      @highs = []
+      @lows = []
+      @forecasts = []
+
+      document.css("simpleforecast forecastday").each_with_index do |forecastday|
+        @days << forecastday.css("date weekday").first.content
+        @highs << forecastday.css('high')
+        @lows << forecastday.css('low')
+        @forecasts << forecastday.css('fcttext')
+      end
+
+      document.css('fcttext').each do |forecast|
+        @forecasts << forecast
       end
     end
 
-  def self.invalid_city?
-    url       = "http://api.wunderground.com/auto/wui/geo/ForecastXML/index.xml?query=#{@location}"
-    document  = Nokogiri::XML(open(url))
-    document.css("simpleforecast forecastday").empty?
+    def self.select_day
+      number = 1
+      
+      puts "Enter The Number For The Day You Want Weather Info For. Type ALL For A Weekly Forecast."
+      
+      @days.each do |day|
+        puts "#{number}) " + day
+        number = number + 1
+      end
+
+      @input = nil
+      @input = gets.strip.downcase
+      puts
+
+      if !self.input_valid?
+        puts "Invalid Choice."
+        self.select_day
+      end
+    end
+
+    def self.input_valid?
+      if (1..6).cover?(@input.to_i)
+        true
+        elsif
+        @input == "all"
+        true
+      else 
+        false
+      end
+    end
+
+
+   def self.single_day_weather
+    puts "In #{@location.capitalize} on #{@days[@input.to_i-1]} there will be a high of #{@highs[@input.to_i-1].css('fahrenheit').first.content} Fahrenheit / #{@highs[@input.to_i-1].css('celsius').first.content} Celsius"
+    puts "The low will be #{@lows[@input.to_i-1].css('fahrenheit').first.content} Fahrenheit / #{@lows[@input.to_i-1].css('celsius').first.content} Celsius"
+    puts @input
+    puts @input
+    puts @input
   end
-
-
-
-  def self.scrape_api
-    url       = "http://api.wunderground.com/auto/wui/geo/ForecastXML/index.xml?query=#{@location}"
-    document  = Nokogiri::XML(open(url))
-    
-    @days = []
-    @highs = []
-    @lows = []
-    @forecasts = []
-
-    document.css("simpleforecast forecastday").each_with_index do |forecastday|
-      @days << forecastday.css("date weekday").first.content
-      @highs << forecastday.css('high')
-      @lows << forecastday.css('low')
-      @forecasts << forecastday.css('fcttext')
-    end
-
-    document.css('fcttext').each do |forecast|
-      @forecasts << forecast
-    end
-  end
-
-  def self.select_day
-    number = 1
-    
-    puts "Enter The Number For The Day You Want Weather Info For. Type ALL For A Weekly Forecast."
-    
-    @days.each do |day|
-      puts "#{number}) " + day
-      number = number + 1
-    end
-
-    @input = nil
-    @input = gets.strip.downcase
-    puts
-
-    if !self.input_valid?
-      puts "Invalid Choice."
-      self.select_day
-    end
-  end
-
-  def self.input_valid?
-    if (1..6).cover?(@input.to_i)
-      true
-      elsif
-      @input == "all"
-      true
-    else 
-      false
-    end
-  end
-
-
-
-
-
 
 
 end
